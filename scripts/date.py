1a7f7952044c56d2e43b34e0fcb6ca7eb9f4186f
Chad Cotton <chad.cotton@outlook.com>
2025-06-04 19:23:02 -0500

    Added straddle matching script.

--- scripts/date.py
+++ scripts/date.py
@@ -4,12 +4,12 @@ from sqlalchemy import create_engine
 def get_next_date(current):
     engine = create_engine("mysql+pymysql://root:password@192.168.1.189/financial_data")
     if current == "":
-        df = pd.read_sql_query("SELECT MIN(date) as 'date' FROM option_chain ORDER BY date DESC LIMIT 1", engine)
+        df = pd.read_sql_query("SELECT MIN(date) as 'date' FROM options ORDER BY date DESC LIMIT 1", engine)
         if df.empty:
             return ""
         return df['date'][0].strftime('%Y-%m-%d')
     else:
-        df = pd.read_sql_query("SELECT MIN(date) as 'date' FROM option_chain WHERE date > '" + current + "'", engine)
+        df = pd.read_sql_query("SELECT MIN(date) as 'date' FROM options WHERE date > '" + current + "'", engine)
         if df.empty:
             return ""
         return df['date'][0].strftime('%Y-%m-%d')
--- scripts/straddle.py
+++ scripts/straddle.py
@@ -11,11 +11,16 @@ class OptionKey:
         self.expiration = expiration
         self.strike = strike
 
+    def __hash__(self):
+        return hash((self.symbol, self.expiration, self.strike))
+
 @dataclass
 class Straddle:
-    def __init__(self, call_id, put_id):
-        self.call_id = call_id
-        self.put_id = put_id
+    def __init__(self, call_option_id, put_option_id, current_date, expiration):
+        self.call_option_id = call_option_id
+        self.put_option_id = put_option_id
+        self.date = current_date
+        self.expiration = expiration
 
 batch_size = 1000
 
@@ -27,24 +32,47 @@ if date == "":
     raise Exception("No data")
 
 while date != "":
-    print("Starting date: " + date)
+    print("Date: " + date)
 
-    chains = pd.read_sql_query("SELECT * FROM option_chain WHERE date = '" + date + "'", engine)
+    chains = pd.read_sql_query("SELECT * FROM options WHERE date = '" + date + "'", engine)
 
     print("Options found: ", len(chains))
 
     straddles = {}
 
-    # Split into call and put options
+    calls = {}
     for call in chains[chains['option_type'] == 0].iterrows():
-        for put in chains[chains['option_type'] == 1].iterrows():
-            if call['symbol'] == put['symbol'] and call['strike'] == put['strike'] and call['expiration'] == put['expiration']:
-                # straddles[OptionKey(call['symbol'], call['expiration'], call['strike'])] = Straddle(call['id'], put['id'])
+        calls[OptionKey(call[1]['symbol'], call[1]['expiration'], call[1]['strike'])] = call
+
+    print("Calls found: ", len(calls))
+
+    puts = {}
+    for put in chains[chains['option_type'] == 1].iterrows():
+        puts[OptionKey(put[1]['symbol'], put[1]['expiration'], put[1]['strike'])] = put
+
+    print("Puts found: ", len(puts))
+
+    for option_id in calls.keys():
+        if option_id in puts:
+            straddles[option_id] = Straddle(calls[option_id][1]['option_id'], puts[option_id][1]['option_id'], date, calls[option_id][1]['expiration'])
+
+    data = []
+
+    for straddle in straddles.values():
+        data.append({
+                'call_option_id': straddle.call_option_id,
+                'put_option_id': straddle.put_option_id,
+                'date': straddle.date,
+                'expiration': straddle.expiration
+            })
+
+    print("Straddles matched: ", len(straddles))
 
     # Batch straddles and insert into database
-    for i in range(0, len(straddles), batch_size):
-        straddles_batch = list(straddles.values())[i:i+batch_size]
-        straddles_df = pd.DataFrame(straddles_batch)
-        straddles_df.to_sql('option_straddles', engine, if_exists='append', index=False)
+    for i in range(0, len(data), batch_size):
+        batch = list(data)[i:i + batch_size]
+        print("Batch: ", int(i / batch_size), " with size: ", len(batch))
+        batch_df = pd.DataFrame(batch)
+        insert = batch_df.to_sql('option_straddle', engine, if_exists='append', index=False)
 
     date = get_next_date(date)
